3f33baaa56252469bafce823cfdb8cae90e8bc9d
gelosani <167290319+gelosani@users.noreply.github.com>
2026-03-17 14:49:28 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 import requests  
 
@@ -14,8 +15,11 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.stop()
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
